fe68cdfcd4f0cfd8f1503e65b2f37d4e571719d3
JayanDesilva <jaycdesilva@gmail.com>
2023-09-08 05:37:41 -0400

    initilizer project added

---
+++ src/main/java/com/gitrepos/housing_eCommerce/HousingECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.gitrepos.housing_eCommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousingECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HousingECommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gitrepos/housing_eCommerce/HousingECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitrepos.housing_eCommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HousingECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
